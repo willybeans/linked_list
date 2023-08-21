1d964f5b8dcadcc0f90c425006082c05e1e738bb
nprock19 <npierok@gmail.com>
2023-08-20 23:02:08 -0400

    added pseeudueo code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*  head (pointer)-> Node1
+    Node1 = next(pointer) -> node2
+            data (pointer to value of a specific type)
+
+
+            Box::Head(Box::Node1(), x)
+*/
+
 fn main() {
     println!("Hello, Nick! :P");
 }
